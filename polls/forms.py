ac6ba67904b6d9bba6c7b3ede81cf4ea75a1ecb3
рума <elchaninapolina@gmail.com>
2024-12-18 20:46:01 +0700

    create models

---
+++ polls/forms.py
@@ -0,0 +1,73 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import CustomUser
+from django.contrib.auth import authenticate
+from django.utils.safestring import mark_safe
+
+
+class CustomUserCreatingForm(forms.ModelForm):
+    username = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Введите имя пользователя'
+        })
+    )
+    email = forms.CharField(
+        label="",
+        max_length=150,
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Введите email'
+        })
+    )
+    first_name = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Введите имя'
+        })
+    )
+    last_name = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Введите фамилию'
+        })
+    )
+    password = forms.CharField(label="",
+                               widget=forms.PasswordInput(attrs={
+            'placeholder': 'Введите пароль'
+        }))
+
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if CustomUser.objects.filter(username=username).exists():
+            raise ValidationError("Такое имя пользователя занято.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if CustomUser.objects.filter(email=email).exists():
+            raise ValidationError("Такой адрес электронной почты занят.")
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            self.add_error('password_confirm', "Пароли не совпадают")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get("password"))
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "first_name", "last_name", 'email')
--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,20 @@
 import datetime
 
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+
+
+class User(AbstractUser):
+    last_name = models.CharField(max_length=254, verbose_name="Фамилия")
+    first_name = models.CharField(max_length=254, verbose_name="Имя")
+    email = models.EmailField(max_length=254, verbose_name="Почта", unique=True)
+    username = models.CharField(max_length=254, verbose_name="Логин", unique=True)
+    password = models.CharField(max_length=254, verbose_name="Пароль")
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Question(models.Model):
@@ -22,5 +35,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-
